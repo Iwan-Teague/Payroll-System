08a7fbbb501fa85481b88189379c9af6766c3aaa
Iwan <teague.iwan@outlook.com>
2024-11-14 18:29:01 +0000

    Add Employee.java class

---
+++ Employee.java
@@ -0,0 +1,333 @@
+public class Employee {
+
+    private String name;
+    private JobCategory jobCategory;
+    private JobType jobRole;
+
+    // Enum to store JobCategory, that stores a unique enum JobType per JobCategory enum
+    public enum JobCategory {
+        presidential {
+            public JobType[] getJobTypes() {
+                return JobType.valuesPresidential();
+            }
+        },
+        academic {
+            public JobType[] getJobTypes() {
+                return JobType.valuesAcademic();
+            }
+        },
+        administrative {
+            public JobType[] getJobTypes() {
+                return JobType.valuesAdministrative();
+            }
+        },
+        education {
+            public JobType[] getJobTypes() {
+                return JobType.valuesEducation();
+            }
+        },
+        library {
+            public JobType[] getJobTypes() {
+                return JobType.valuesLibrary();
+            }
+        },
+        it {
+            public JobType[] getJobTypes() {
+                return JobType.valuesIT();
+            }
+        },
+        technical {
+            public JobType[] getJobTypes() {
+                return JobType.valuesTechnical();
+            }
+        },
+        serviceStaff {
+            public JobType[] getJobTypes() {
+                return JobType.valuesServiceStaff();
+            }
+        },
+        teachers {
+            public JobType[] getJobTypes() {
+                return JobType.valuesTeachers();
+            }
+        },
+        clinical {
+            public JobType[] getJobTypes() {
+                return JobType.valuesClinical();
+            }
+        },
+        ulac {
+            public JobType[] getJobTypes() {
+                return JobType.valuesULAC();
+            }
+        },
+        research {
+            public JobType[] getJobTypes() {
+                return JobType.valuesResearch();
+            }
+        };
+
+        //use abstract to ensure every JobCategory impliments getJobTypes()
+        public abstract JobType[] getJobTypes();
+    }
+
+    // Enum for all JobTypes
+    public enum JobType {
+        // Presidential category
+        president,
+        vicePresident,
+
+        // Academic category
+        fullProfessor,
+        professorFormerlyAssociateProfessor,
+        associateProfessorAFormerlySeniorLecturer,
+        associateProfessorBFormerlyLecturer,
+        assistantProfessorFormerlyLecturerBelowTheBar,
+        teachingAssistant,
+
+        // Administrative category
+        seniorAdministrativeOfficer3,
+        seniorAdministrativeOfficer2,
+        seniorAdministrativeOfficer1,
+        seniorAdministrativeAdministrator,
+        executiveAdministrator,
+        seniorAdministrator,
+        administrator,
+
+        // Education category
+        epsPortfolioManager,
+        epsCategoryManager,
+        epsCategorySpecialistHigher,
+        epsCategorySpecialist,
+
+        // Library category
+        subLibrarian,
+        assistantLibrarian2,
+        assistantLibrarian1,
+        seniorLibraryAssistant,
+        libraryAssistant,
+        libraryAttendant,
+
+        // IT category
+        analystProgrammer3,
+        analystProgrammer2,
+        analystProgrammer1,
+        seniorComputerOperator,
+        computerOperator,
+        printOperator2,
+        printOperator1,
+        computerLabAttendant,
+        temporaryComputerAssistant,
+
+        // Technical category
+        chiefTechnicalOfficer,
+        technicalOfficer,
+        seniorTechnicalOfficer,
+        seniorLabAttendant,
+        laboratoryAttendant,
+
+        // Service Staff category
+        senPorterAttendant,
+        porterAttendant,
+        groundsSupervisor,
+        groundsWorkPerson,
+        seniorAide,
+        groundsWorkPersonMachineAttendant,
+        serviceStaff,
+        serviceStaffShift,
+        plantMaintenanceAide,
+        groundsForePerson,
+
+        // Teachers category
+        teachingFellow,
+        universityTeacher,
+        associateTeacher,
+
+        // Clinical category
+        therapiesRegionalSupervisorsRegionalPlacementFacilitator,
+        clinicalTutor,
+        clinicalFellow,
+
+        // ULAC category
+        assistantSeniorInstructor,
+        leadInstructor,
+        multiActivityInstructorGrade1,
+        multiActivityInstructorGrade2,
+        assistantInstructor,
+        coopStudents,
+
+        // Research category
+        researchAssistant,
+        postDoctoralResearcher,
+        researchFellow,
+        seniorResearchFellow;
+
+        // Methods to link correct jobRoles to correct jobCatagories
+        public static JobType[] valuesPresidential() {
+            return new JobType[]{ president, vicePresident };
+        }
+
+        public static JobType[] valuesAcademic() {
+            return new JobType[]{
+                    fullProfessor,
+                    professorFormerlyAssociateProfessor,
+                    associateProfessorAFormerlySeniorLecturer,
+                    associateProfessorBFormerlyLecturer,
+                    assistantProfessorFormerlyLecturerBelowTheBar,
+                    teachingAssistant
+            };
+        }
+
+        public static JobType[] valuesAdministrative() {
+            return new JobType[]{
+                    seniorAdministrativeOfficer3,
+                    seniorAdministrativeOfficer2,
+                    seniorAdministrativeOfficer1,
+                    seniorAdministrativeAdministrator,
+                    executiveAdministrator,
+                    seniorAdministrator,
+                    administrator
+            };
+        }
+
+        public static JobType[] valuesEducation() {
+            return new JobType[]{
+                    epsPortfolioManager,
+                    epsCategoryManager,
+                    epsCategorySpecialistHigher,
+                    epsCategorySpecialist
+            };
+        }
+
+        public static JobType[] valuesLibrary() {
+            return new JobType[]{
+                    subLibrarian,
+                    assistantLibrarian2,
+                    assistantLibrarian1,
+                    seniorLibraryAssistant,
+                    libraryAssistant,
+                    libraryAttendant
+            };
+        }
+
+        public static JobType[] valuesIT() {
+            return new JobType[]{
+                    analystProgrammer3,
+                    analystProgrammer2,
+                    analystProgrammer1,
+                    seniorComputerOperator,
+                    computerOperator,
+                    printOperator2,
+                    printOperator1,
+                    computerLabAttendant,
+                    temporaryComputerAssistant
+            };
+        }
+
+        public static JobType[] valuesTechnical() {
+            return new JobType[]{
+                    chiefTechnicalOfficer,
+                    technicalOfficer,
+                    seniorTechnicalOfficer,
+                    seniorLabAttendant,
+                    laboratoryAttendant
+            };
+        }
+
+        public static JobType[] valuesServiceStaff() {
+            return new JobType[]{
+                    senPorterAttendant,
+                    porterAttendant,
+                    groundsSupervisor,
+                    groundsWorkPerson,
+                    seniorAide,
+                    groundsWorkPersonMachineAttendant,
+                    serviceStaff,
+                    serviceStaffShift,
+                    plantMaintenanceAide,
+                    groundsForePerson
+            };
+        }
+
+        public static JobType[] valuesTeachers() {
+            return new JobType[]{
+                    teachingFellow,
+                    universityTeacher,
+                    associateTeacher
+            };
+        }
+
+        public static JobType[] valuesClinical() {
+            return new JobType[]{
+                    therapiesRegionalSupervisorsRegionalPlacementFacilitator,
+                    clinicalTutor,
+                    clinicalFellow
+            };
+        }
+
+        public static JobType[] valuesULAC() {
+            return new JobType[]{
+                    assistantSeniorInstructor,
+                    leadInstructor,
+                    multiActivityInstructorGrade1,
+                    multiActivityInstructorGrade2,
+                    assistantInstructor,
+                    coopStudents
+            };
+        }
+
+        public static JobType[] valuesResearch() {
+            return new JobType[]{
+                    researchAssistant,
+                    postDoctoralResearcher,
+                    researchFellow,
+                    seniorResearchFellow
+            };
+        }
+    }
+
+    // Constructor
+    //pull data from excel sheet and pass name, jobCategory and jobRole. jobCategory and jobRole are enums.
+    public Employee(String name, JobCategory jobCategory, JobType jobRole) {
+        if (!isRoleValidForCategory(jobCategory, jobRole)) {
+            throw new IllegalArgumentException("Invalid JobRole for the given JobCategory");
+        }
+        this.name = name;
+        this.jobCategory = jobCategory;
+        this.jobRole = jobRole;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Getter for jobCategory
+    public JobCategory getJobCategory() {
+        return jobCategory;
+    }
+
+    // Getter for jobRole
+    public JobType getJobRole() {
+        return jobRole;
+    }
+
+    // Check if the jobRole is valid for the given jobCategory
+    private boolean isRoleValidForCategory(JobCategory jobCategory, JobType jobRole) {
+        for (JobType validJobRole : jobCategory.getJobTypes()) {
+            if (validJobRole == jobRole) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", jobCategory=" + jobCategory +
+                ", jobRole=" + jobRole +
+                '}';
+    }
+}
