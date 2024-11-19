dfb5ef1b093a63940a7a6ec1d9d6ac72cff59ea4
Samuel Luke <108830145+Snoopytoop@users.noreply.github.com>
2024-11-19 18:26:10 +0000

    changed datecheck so it only promotes and issues payslips once in each cycle

--- DateCheck.java
+++ DateCheck.java
@@ -2,13 +2,16 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DateCheck {
-    private static String getFormattedDate(String pattern) {
+    private boolean completedPayslips = false;
+    private boolean completedPromotion = false;
+
+    private String getFormattedDate(String pattern) {
         LocalDate currentDate = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
         return currentDate.format(formatter);
     }
 
-    public static boolean check(String type) {
+    private boolean check(String type) {
         String month = getFormattedDate("MM");
         String day = getFormattedDate("dd");
 
@@ -18,4 +21,25 @@ public class DateCheck {
             default -> throw new IllegalArgumentException("Invalid type: " + type);
         };
     }
+
+    public void runPayslips() {
+        if ((check("payslip") == true) && (completedPayslips == false)) {
+            PayslipGenerator payslipGenerator = new PayslipGenerator();
+            payslipGenerator.writeCsv();
+            completedPayslips = true;
+        } else if (check("payslip") == false) {
+            completedPayslips = false;
+        }
+    }
+
+    public void runPromotion() {
+        if ((check("promotion") == true) && (completedPromotion == false)) {
+            PayscalePromoter payscalePromoter = new PayscalePromoter();
+            payscalePromoter.writeToCSV();
+            completedPromotion = true;
+        } else if (check("promotion") == false) {
+            completedPromotion = false;
+        }
+    }
+
 }
