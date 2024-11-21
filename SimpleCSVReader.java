fbdf369a45a5664856de7f5a8f8f68a3f4c4feeb
Saif-Khawaldeh <80422866+Saif-Khawaldeh@users.noreply.github.com>
2024-11-21 19:35:43 +0300

    Updated SimpleCSVReader.java
    now all classes should access SimpleCSVReader

--- SimpleCSVReader.java
+++ SimpleCSVReader.java
@@ -27,5 +27,62 @@ public class SimpleCSVReader {
 
         return employees;
     }
-}
 
+    public List<String[]> readCsvPaySlip() {
+        List<String[]> rows = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("Employees.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(",");
+
+                if (fields.length > 5) {
+                    String[] selectedFields = {fields[0], fields[1], fields[5]};
+                    rows.add(selectedFields);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return rows;
+    }
+    public List<String[]> readCsvPayScale(int length, String name) {
+        List<String[]> rows = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(name))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(",");
+                if (fields.length > length) {
+                    rows.add(fields);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return rows;
+    }
+    public List<Employee> readCSVSalary(String var1) throws IOException {
+        ArrayList var2 = new ArrayList();
+        EmployeeMapper var3 = new EmployeeMapper();
+        BufferedReader var4 = new BufferedReader(new FileReader(var1));
+
+        try {
+            var4.readLine();
+
+            String var5;
+            while((var5 = var4.readLine()) != null) {
+                var2.add(var3.fromCSV(var5));
+            }
+        } catch (Throwable var8) {
+            try {
+                var4.close();
+            } catch (Throwable var7) {
+                var8.addSuppressed(var7);
+            }
+
+            throw var8;
+        }
+
+        var4.close();
+        return var2;
+    }
+}
