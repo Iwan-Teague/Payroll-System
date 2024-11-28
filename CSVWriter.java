f47eb9cb9d952c87d42182186e9517955ec19ae6
Simon Alexander <simonalex957@gmail.com>
2024-11-28 18:15:54 +0000

    99% finished admin menu, will finish tommorow

--- CSVWriter.java
+++ CSVWriter.java
@@ -33,4 +33,24 @@ public class CSVWriter {
         }
     }
 
+    public void writeToCSV(String filePath, String... values) {
+        try (FileWriter writer = new FileWriter(filePath, true)) {
+            // Convert the array of values to a CSV line
+            StringBuilder csvLine = new StringBuilder();
+            for (int i = 0; i < values.length; i++) {
+                csvLine.append(values[i]);
+
+                // Add a comma unless it's the last value
+                if (i < values.length - 1) {
+                    csvLine.append(",");
+                }
+            }
+
+            // Write the line and a newline to the CSV
+            writer.append(csvLine.toString()).append("\n");
+        } catch (IOException e) {
+            System.err.println("Error writing to CSV file: " + e.getMessage());
+        }
+    }
+
 }
