c8bffe3d1660691f367ec05918a90f741ea6c505
Iwan-Teague <teague.iwan@outlook.com>
2024-12-01 13:38:19 +0000

    Update main.java
    call main method in cli.

--- main.java
+++ main.java
@@ -5,7 +5,7 @@ public class main {
     public static void main(String[] args) {
         DateCheck dateCheck = new DateCheck();
         CLI cli = new CLI();
-        cli.startScreen();
+        cli.main(args);
         
     }
 }
