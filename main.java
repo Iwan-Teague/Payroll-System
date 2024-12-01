683c2e9077dee892ca10b48ce6b6df286149e77b
Saif-Khawaldeh <80422866+Saif-Khawaldeh@users.noreply.github.com>
2024-12-01 05:10:16 +0300

    Updated to run DateCheck and CLI main.java

--- main.java
+++ main.java
@@ -2,8 +2,9 @@
 
 public class main {
     public static void main(String[] args) {
-
+        DateCheck dateCheck = new DateCheck();
+        CLI cli = new CLI();
+        cli.startScreen();
         
-
     }
 }
